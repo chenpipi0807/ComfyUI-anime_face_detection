f266309d2af622afbcf62a0c522882d58489fded
chenpipi0807 <144689950+chenpipi0807@users.noreply.github.com>
2025-02-17 14:37:09 +0800

    Update __init__.py
    新功能人脸检测+图像随机拼贴

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
 from .PIP_AnimeFaceDetect import PIP_AnimeFaceDetect
+from .collage import PIP_Collage
 
+# 节点映射配置
 NODE_CLASS_MAPPINGS = {
     "PIP_AnimeFaceDetect": PIP_AnimeFaceDetect,
+    "PIP_Collage": PIP_Collage
 }
 
+# 节点显示名称映射
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PIP_AnimeFaceDetect": "PIP 动漫人脸检测",
+    "PIP_Collage": "PIP 随机拼图"
 }
