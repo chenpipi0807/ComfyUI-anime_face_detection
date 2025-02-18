949e1e22642ce37add8da78e7dceb913d4458b09
chenpipi0807 <144689950+chenpipi0807@users.noreply.github.com>
2025-02-18 13:28:32 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 from .PIP_AnimeFaceDetect import PIP_AnimeFaceDetect
 from .collage import PIP_Collage
 
-# 节点映射配置
+# Node class mappings
 NODE_CLASS_MAPPINGS = {
     "PIP_AnimeFaceDetect": PIP_AnimeFaceDetect,
     "PIP_Collage": PIP_Collage
 }
 
-# 节点显示名称映射
+# Node display name mappings
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PIP_AnimeFaceDetect": "PIP 动漫人脸检测",
     "PIP_Collage": "PIP 随机拼图"
