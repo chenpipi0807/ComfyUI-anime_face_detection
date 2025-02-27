498aa04a64ce53c06995901fed0e9386487dcdad
chenpipi0807 <144689950+chenpipi0807@users.noreply.github.com>
2025-02-27 18:54:12 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,17 @@
 from .PIP_AnimeFaceDetect import PIP_AnimeFaceDetect
 from .collage import PIP_Collage
+from .grid_comic import PIP_GridComic
 
 # Node class mappings
 NODE_CLASS_MAPPINGS = {
     "PIP_AnimeFaceDetect": PIP_AnimeFaceDetect,
-    "PIP_Collage": PIP_Collage
+    "PIP_Collage": PIP_Collage,
+    "PIP_GridComic": PIP_GridComic
 }
 
 # Node display name mappings
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PIP_AnimeFaceDetect": "PIP 动漫人脸检测",
-    "PIP_Collage": "PIP 随机拼图"
+    "PIP_Collage": "PIP 随机拼图",
+    "PIP_GridComic": "PIP 格漫拼图"
 }
